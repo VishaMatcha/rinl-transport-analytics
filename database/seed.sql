044163366503555953c935eaf1a7c26941dc8224
Vishal <matchavishal@gmail.com>
2025-02-14 12:36:47 -0800

    5

--- database/seed.sql
+++ database/seed.sql
@@ -1,4 +1,10 @@
 INSERT INTO railway_data (train_no, train_name, source, destination, departure_time, arrival_time, travel_time, distance)
 VALUES 
 ('12723', 'TELANGANA EXP', 'HYD', 'NDLS', '06:30', '09:05', '26:35', 1700),
-('12295', 'SANGHAMITRA EXP', 'YPR', 'DDN', '09:00', '07:45', '46:45', 2481);
+('12295', 'SANGHAMITRA EXP', 'YPR', 'DDN', '09:00', '07:45', '46:45', 2481),
+('12603', 'MAS-NDLS EXPRESS', 'MAS', 'NDLS', '18:10', '22:45', '28:35', 2180),
+('12565', 'BIHAR SAMPARK KRANTI', 'DNR', 'NDLS', '20:15', '10:00', '13:45', 1086),
+('12497', 'SHANE PUNJAB EXPRESS', 'ASR', 'NDLS', '03:10', '12:20', '9:10', 448),
+('12002', 'BHOPAL SHATABDI', 'NDLS', 'BPL', '06:00', '14:55', '8:55', 707),
+('22691', 'RAJDHANI EXP', 'SBC', 'NZM', '20:00', '05:50', '33:50', 2365),
+('12244', 'DURONTO EXPRESS', 'ADI', 'MMCT', '23:25', '05:50', '6:25', 490);
